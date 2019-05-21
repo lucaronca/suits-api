ad9ccf56663c45459913a2782acf2bf3d02f4080
Luca Ronca <luca.ronca07@gmail.com>
2019-05-21 14:44:24 +0200

    feat: add drop_suits command

---
+++ apps/scraped_suits/management/commands/drop_suits.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+from apps.scraped_suits.models import Suit
+
+class Command(BaseCommand):
+    help = 'Drop all suits in the database'
+
+    def handle(self, *args, **options):
+        Suit.objects.all().delete()
