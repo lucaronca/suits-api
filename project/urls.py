c9bbe2b6a4d43c06004ce03ad81d9a0ade36135d
Luca Ronca <luca.ronca07@gmail.com>
2019-05-21 14:21:16 +0200

    feat: add suits api endpoint

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
-
 
 
 router = routers.DefaultRouter()
@@ -9,4 +8,5 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    re_path('api/(?P<version>(v1|v2))/', include('apps.scraped_suits.urls'))
 ]
