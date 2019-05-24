3943aad5ebabd689cf05ebf3b1cb789c5634a140
Luca Ronca <luca.ronca07@gmail.com>
2019-05-24 14:10:23 +0200

    feat: make suit url primary key

--- apps/scraped_suits/models.py
+++ apps/scraped_suits/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Suit(models.Model):
-    url = models.CharField(max_length=1000)
+    url = models.CharField(max_length=1000, primary_key=True)
     name = models.CharField(max_length=150)
     color = models.CharField(max_length=150)
     fit = models.CharField(max_length=150)
