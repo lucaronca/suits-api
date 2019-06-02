e4daead99c2c107d796371ebe70549b349d305fd
Luca Ronca <luca.ronca07@gmail.com>
2019-06-02 02:16:33 +0200

    feat: replace root endpoint view with api_root view

--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import routers
+from .views import api_root
 
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', api_root),
     path('admin/', admin.site.urls),
     re_path('api/(?P<version>(v1|v2))/', include('apps.scraped_suits.urls'))
 ]
