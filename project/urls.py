8d774041d97e70e543941d02ac06e6f464d78509
Luca Ronca <luca.ronca07@gmail.com>
2019-06-02 02:17:58 +0200

    feat: add StatusCheckView to status-check endpoint

--- project/urls.py
+++ project/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import routers
-from .views import api_root
+from .views import api_root, StatusCheckView
 
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
     path('', api_root),
+    path('status-check', StatusCheckView.as_view()),
     path('admin/', admin.site.urls),
     re_path('api/(?P<version>(v1|v2))/', include('apps.scraped_suits.urls'))
 ]
