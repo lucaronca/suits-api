28c8f19b320c7625d7a041b5fcbbf22a6609c7f9
Luca Ronca <luca.ronca07@gmail.com>
2019-06-06 22:27:21 +0200

    feat: handle price items as well as already managed suits items in SuitsPipeline

--- services/scraper/scraper/pipelines.py
+++ services/scraper/scraper/pipelines.py
@@ -1,30 +1,45 @@
-from apps.scraped_suits.models import Suit
+from apps.scraped_suits.models import Suit, Price
+from .items import SuitItem, PriceItem
 
 
-class SuitPipeline():
+class SuitsPipeline():
     def process_item(self, item, spider):
+        if isinstance(item, SuitItem):
+            return self.handle_suit(item, spider)
+        if isinstance(item, PriceItem):
+            return self.handle_price(item, spider)
+
+        raise Exception('item of {} Class is not handled by {}'.format(type(item).__name__, self.__class__.__name__))
+
+    def handle_suit(self, item, spider):
         try:
             # suit already exits
             suit = Suit.objects.get(pk=item['url'])
-            field_to_update = None
+            field_to_update = []
 
             # attribute spiders
-            if item.get('color', None) != None:
+            if item.get('color'):
                 suit.color = item['color']
-                field_to_update = 'color'
-            if item.get('fit', None) != None:
+                field_to_update.append('color')
+            if item.get('fit'):
                 suit.fit = item['fit']
-                field_to_update = 'fit'
-            if item.get('material', None) != None:
+                field_to_update.append('fit')
+            if item.get('material'):
                 suit.material = item['material']
-                field_to_update = 'material'
+                field_to_update.append('material')
 
-            # update the model if pipeline has processed an attribute spider
+            # update the model if pipeline processed an attribute spider
             if field_to_update != None:
-                suit.save(update_fields=[field_to_update])
+                suit.save(update_fields=field_to_update)
 
         except Suit.DoesNotExist:
             item.save() # save instantiates the django model and save it
 
         finally:
             return item
+
+    def handle_price(self, item, spider):
+        suit = Suit.objects.get(pk=item['url'])
+        price = Price(suit=suit, amount=item.get('amount'), currency=item.get('currency'))
+        price.save()
+        return item
