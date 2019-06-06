ff0e2bbb54eaafebcc6a38bcf4ea12b787cc6b95
Luca Ronca <luca.ronca07@gmail.com>
2019-06-06 22:24:35 +0200

    feat: define PriceItem item in scraper

--- services/scraper/scraper/items.py
+++ services/scraper/scraper/items.py
@@ -1,6 +1,11 @@
+import scrapy
 from scrapy_djangoitem import DjangoItem
-from apps.scraped_suits.models import Suit
+from apps.scraped_suits.models import Suit, Price
 
 
 class SuitItem(DjangoItem):
     django_model = Suit
+
+class PriceItem(DjangoItem):
+    django_model = Price
+    url = scrapy.Field()
